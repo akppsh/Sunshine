95c83e03b73bfed88feed5746818af546ad6815e
akppsh <abhi11293@gmail.com>
2016-10-01 18:31:00 +0530

    Added ForecastFragement Class

--- app/src/main/java/com/techkona/sunshine/MainActivity.java
+++ app/src/main/java/com/techkona/sunshine/MainActivity.java
@@ -1,15 +1,11 @@
 package com.techkona.sunshine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,23 +17,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        // Array of strings...
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item_forecast, data);
-
-        ListView listView = (ListView) findViewById(R.id.listview_forecast);
-        listView.setAdapter(adapter);
-
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.content_main, new ForecastFragment())
+                    .commit();
+        }
     }
 
     @Override
